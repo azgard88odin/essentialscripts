c297c3edf72556de821afaa74d9d2e14ecaa1b66
JJ <156287550+azgard88odin@users.noreply.github.com>
2024-07-23 17:41:22 +0200

    Delete cmDisable.cmd
    cmprivacy upgraded

--- cmDisable.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-powershell.exe -Command "& {Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone' -Name 'Value' -Value "Deny"}"
-powershell.exe -Command "& {Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\webcam' -Name 'Value' -Value "Deny"}"
