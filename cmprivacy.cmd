0810cb75df56aaf8587574607b5c32b856228687
JJ <156287550+azgard88odin@users.noreply.github.com>
2024-07-23 17:42:25 +0200

    Update cmprivacy.cmd
    new upgrade

--- cmprivacy.cmd
+++ cmprivacy.cmd
@@ -1,11 +1,16 @@
 @echo off
 
+SET "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/c cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
 SET option=%1
 
 if "%option%"=="/e" (
-	powershell.exe -Command "& Start-Process -FilePath .\cmEnable.cmd -Verb RunAs"
+	reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone" /v "Value" /t REG_SZ /d "Allow" /f
+	reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\webcam" /v "Value" /t REG_SZ /d "Allow" /f
 ) else if "%option%"=="/d" (
-	powershell.exe -Command "& Start-Process -FilePath .\cmDisable.cmd -Verb RunAs"
+	reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone" /v "Value" /t REG_SZ /d "Deny" /f
+	reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\webcam" /v "Value" /t REG_SZ /d "Deny" /f
 ) else (
 	echo Usage: %0 [/e "enable"^|/d "disable"]
 )
