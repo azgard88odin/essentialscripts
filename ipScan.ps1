c76e9393a664f38a5eebae04743a020849752646
JJ <156287550+azgard88odin@users.noreply.github.com>
2024-12-04 14:40:02 +0200

    Add files via upload

---
+++ ipScan.ps1
@@ -0,0 +1,26 @@
+﻿$subnet = "192.168.1"
+$report = @()
+
+# Loop through all IP addresses from 1 to 254 in the range
+for ($i = 1; $i -le 254; $i++) {
+    $ip = "$subnet.$i"
+    Write-Host "Testing $ip"
+
+    $pingResult = Test-Connection -ComputerName $ip -Count 1 -Quiet
+    if ($pingResult) {
+        $report += "$ip is UP"
+    }
+}
+
+# Print the report
+if ($report.Count -gt 0) {
+    $hostCount = $report.Count
+    Write-Host ""
+    Write-Host "REPORT:"
+    Write-Host "-----------------------------------"
+    Write-Host "$hostCount Hosts are UP"
+    Write-Host "-----------------------------------"
+    $report | ForEach-Object { Write-Host $_ }
+} else {
+    Write-Host "No devices are responding."
+}
---
+++ regexExtract.ps1
@@ -0,0 +1,22 @@
+﻿# This script was to help create a lookup sheet for dated files. Each photo filename has a date in it
+# Essentially, there are thousands upon thousands of archived images but the dates on the subfolders, were incorrect
+# Example: the Folder '2024-10-11' would contain photos from 2024-10-11,12 and, 13
+# This made it difficult for me to see if photos from certain dates were even saved in the archive.
+# With this I extracted a list of all the dates in the filenames so that I can quickly search a date range
+# With that information it was easier to track down the possible subfolder in which it was contained, which avoided iterating through thousands of folders to find a set of files
+$directory = "\path\to\directory"
+$regex = '20240\d{3}'
+
+$uniqueStrings = @()
+
+Get-ChildItem -Path $directory -Recurse -File | ForEach-Object {
+    $filename = [System.IO.Path]::GetFileName($_)
+    $matches = [regex]::Matches($filename, $regex)
+    foreach ($match in $matches) {
+        if ($uniqueStrings -notcontains $match.Value) {
+            $uniqueStrings += $match.Value
+        }
+    }
+}
+
+Write-Host $uniqueStrings
